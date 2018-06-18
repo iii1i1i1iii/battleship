caff884a72fec368aecb32648328acc26b9b1d17
Ivan Mykhailenko <hbzzse1@gmail.com>
2018-06-18 17:57:34 +0300

    First commit. Add all files

---
+++ battleship.js
@@ -0,0 +1,25 @@
+var view = {
+  displayMessege: function(msg) {
+    var massegeArea = document.getElementById("massegeArea");
+    massegeArea.innerHTML = msg;
+  },
+  displayHit: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  }
+  };
+
+
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessege("Tap tap, is this thing on?");
