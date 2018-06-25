f93c824824ad0e6bf68b72c7774d44cd9c46d364
Ivan Mykhailenko <hbzzse1@gmail.com>
2018-06-25 19:17:17 +0300

    25/06 - add small part from work

--- battleship.js
+++ battleship.js
@@ -12,8 +12,27 @@ var view = {
     cell.setAttribute("class", "miss");
   }
   };
-
-
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLenght: 3,
+  shipsSunk: 0,
+  ships: [{location: [06, 16, 26], hits: ["", "", ""] },
+          {location: [24, 34, 44], hits: ["", "", ""] },
+          {location: [10, 11, 12], hits: ["", "", ""] }],
+  fire: function(guess){
+    for (var i = 0; i < this.numShips; i++) {
+      var ships = this.ships[i];
+      var location = this.location;
+      var index = location.indexOf(guess);
+        if (index >= 0) {
+          ship.hits = "hit";
+          return true;
+        }
+    }
+    return false;
+  }
+};
 
 view.displayMiss("00");
 view.displayHit("34");
