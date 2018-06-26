52a75d5008ec7ec1f306d4411a18e0eeb479b8d5
Ivan Mykhailenko <hbzzse1@gmail.com>
2018-06-26 19:34:15 +0300

    page 372

--- battleship.js
+++ battleship.js
@@ -23,23 +23,46 @@ var model = {
   fire: function(guess){
     for (var i = 0; i < this.numShips; i++) {
       var ships = this.ships[i];
-      var location = this.location;
-      var index = location.indexOf(guess);
+      var index = ship.location.indexOf(guess);
         if (index >= 0) {
           ship.hits = "hit";
+          view.displayHit(guess);
+          view.displayMessege("HIT")
+          if (this.isSunk(ship)) {
+            view.displayMessege("You sunk my battleship");
+            this.shipsSunk++;
+          }
           return true;
         }
     }
+    view.displayMiss(guess);
+    view.displayMessege("You missed.");
     return false;
+  },
+  isSunk: function(ship) {
+
   }
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
+model.fire("53");
+
+model.fire("06");
+model.fire("16");
+model.fire("26");
+
+model.fire("34");
+model.fire("24");
+model.fire("44");
+
+model.fire("12");
+model.fire("11");
+model.fire("10");
+  
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMiss("55");
+// view.displayHit("12");
+// view.displayMiss("25");
+// view.displayHit("26");
 
 view.displayMessege("Tap tap, is this thing on?");
-// ss
