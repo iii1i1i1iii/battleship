d6b65297fd56caa2a340a6153b44357b17f40ddf
Ivan Mykhailenko <hbzzse1@gmail.com>
2018-06-27 18:42:25 +0300

    377 model was finished and added some new code to controller

--- battleship.js
+++ battleship.js
@@ -17,17 +17,18 @@ var model = {
   numShips: 3,
   shipLenght: 3,
   shipsSunk: 0,
-  ships: [{location: [06, 16, 26], hits: ["", "", ""] },
-          {location: [24, 34, 44], hits: ["", "", ""] },
-          {location: [10, 11, 12], hits: ["", "", ""] }],
+  ships: [{locations: ["06", "16", "26"], hits: ["", "", ""] },
+          {locations: ["24", "34", "44"], hits: ["", "", ""] },
+          {locations: ["10", "11", "12"], hits: ["", "", ""] }],
   fire: function(guess){
     for (var i = 0; i < this.numShips; i++) {
-      var ships = this.ships[i];
-      var index = ship.location.indexOf(guess);
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+      console.log(index);
         if (index >= 0) {
           ship.hits = "hit";
           view.displayHit(guess);
-          view.displayMessege("HIT")
+          view.displayMessege("HIT");
           if (this.isSunk(ship)) {
             view.displayMessege("You sunk my battleship");
             this.shipsSunk++;
@@ -43,6 +44,23 @@ var model = {
 
   }
 };
+var controller = {
+  guesses: 0,
+  processGuesses: function (guess) {
+    // Kod metoda
+  }
+}
+
+function parseGuesses(guess) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+    if (guess == null || guess.length !== 2) {
+      alert("Opps, please enter a letter and number on the board.")
+    } else {
+      firstChar = guess.charAt(0);
+      var row = alphabet.indexOf(firstChar);
+      var column = guess.charAt(1);
+    }
+}
 
 model.fire("53");
 
@@ -57,7 +75,7 @@ model.fire("44");
 model.fire("12");
 model.fire("11");
 model.fire("10");
-  
+
 // view.displayMiss("00");
 // view.displayHit("34");
 // view.displayMiss("55");
